f28f342c12ac0f0d619295e3ad16f147c264c806
caoru828 <18846432589@163.com>
2017-12-08 21:20:37 +0800

    php99乘法表

--- testa/test6.php
+++ testa/test6.php
@@ -48,3 +48,25 @@ for($i=1;$i<10;$i++)
 
 }
 echo "</table>";
+
+
+echo "<table width=800 border=1 align='center'>";
+for($i=9;$i>0;$i--)
+{
+    echo "<tr>";
+
+    for ($k=1;$k<=9-$i;$k++){
+        echo "<td>";
+        echo"&nbsp;";
+        echo "</td>";
+    }
+    for($j=1;$j<=10-$k;$j++){
+        echo "<td>";
+        echo "$i"."*"."$j "." = ".$i*$j;
+        echo "</td>";
+    }
+    echo "<br>";
+    echo "</tr>";
+
+}
+echo "</table>";
