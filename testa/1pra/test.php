be0b119ee94e8ff0efcdc0083332d6d7acaee28f
caoru828 <18846432589@163.com>
2017-12-19 15:08:28 +0800

    php练习题练习

--- testa/1pra/test.php
+++ testa/1pra/test.php
@@ -5,10 +5,23 @@ $two = &$one;
 $one =10;
 $two = "hello";
 
-
-
 echo $one."<br/>";
 echo $two."<br/>";
+
+$a = 20;
+//$b=$a++;
+$b = $a++ + ++$a;//20 + 22
+echo $a;  //22
+echo $b;  //42
+$c = --$b - $b--;//41 - 41
+echo "<br/>".$b."<br/>"; //40
+echo $c."<br/>";//0
+$d = $c++ + --$c;// 0 + 0
+echo "<br/>".$c."<br/>";  //0
+echo $d."<br/>";          //0
+//echo $d;
+
+
 ?>
 <script>
 //直接赋值       引用赋值
