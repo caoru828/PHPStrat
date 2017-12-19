3657df8f4432879ac242459037d4ac4d7f69e1ba
caoru828 <18846432589@163.com>
2017-12-19 23:52:06 +0800

    php练习题练习

--- testa/1pra/bianzhi.php
+++ testa/1pra/bianzhi.php
@@ -7,7 +7,7 @@
 <body>
 <p>sddfca</p>
 <script>
-//    直接赋值  引用赋值
+    //    直接赋值  引用赋值
     var name = 123;
 
     var obj = {
@@ -23,12 +23,27 @@
 </html>
 
 <?php
-    $one = 100;
-    $two = &$one;
-    echo $two;//100
+$one = 100;
+$two = &$one;
+echo $two;//100
 
-    $one = 10;
-    $two = "hello";
-    echo $two;//hello
-    echo $one;//hello
+$one = 10;
+$two = "hello";
+echo $two;//hello
+echo $one;//hello
+
+//  js不可以用关键字命名，php可以
+
+
+//可变变量
+$one ="$$$$";
+$two = "one";
+$three = "two";
+$four = "three";
+
+echo $four;//three
+echo $$four;//two
+
+
+
 ?>
