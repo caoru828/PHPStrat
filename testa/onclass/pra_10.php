a45acfe364c36da77b63e9558825cf2708cbfbe5
caoru828 <18846432589@163.com>
2017-12-20 16:14:14 +0800

    php练习题练习

--- testa/onclass/pra_10.php
+++ testa/onclass/pra_10.php
@@ -1,33 +1,59 @@
 <?php
 for($i=1;$i<=5;$i++){
-    for ($j=0;$j<5-$i;$j++){
+    for($j=5-$i;$j>=0;$j--){
         echo "&nbsp;";
     }
-    for($k=0;$k<2*$i-1;$k++){
+    for($k=1;$k<=2*$i-1;$k++){
         echo "* ";
     }
     echo "<br>";
 }
 
 for($i=1;$i<=5;$i++){
-    for ($j=0;$j<5-$i;$j++){
+    for($j=5-$i;$j>=0;$j--){
         echo "&nbsp;";
     }
-        for($k=0;$k<2*$i-1;$k++){
-        if ($k==1||$k==5){
+    if($i==5){
+        for ($m=1;$m<=9;$m++){
             echo "* ";
-        }else{
-            if ($k==1||$k==2*$i-1){
-                echo '*';
+        }
+    }else{
+        for($k=1;$k<=2*$i-1;$k++){
+            if($k==1||$k==(2*$i-1)){
+                echo "* ";
             }else{
                 echo "&nbsp;";
             }
 
         }
-
     }
+
     echo "<br>";
 }
+
+$n=5;
+for($i=1;$i<=$n;$i++){
+    for($k=1;$k<=$n-$i;$k++){
+        echo "&nbsp;";
+    }
+
+    for($j=1;$j<=2*$i-1;$j++){
+        if($i==1 || $i==$n){
+            echo "*";
+        }else{
+            if($j==1 || $j==2*$i-1){
+                echo "*";
+            }else{
+                echo "&nbsp;";
+            }
+        }
+
+    }
+    echo "<br />";
+
+}
+
+
 
 ?>
 
