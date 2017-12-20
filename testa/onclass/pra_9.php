ecf8d84e42f485b5a44b82c694a5cc874282dc26
caoru828 <18846432589@163.com>
2017-12-20 15:40:13 +0800

    php练习题练习

--- testa/onclass/pra_9.php
+++ testa/onclass/pra_9.php
@@ -1,16 +1,27 @@
 <?php
-
-
 for($i=1;$i<=9;$i++){
-    for ($j=1;$j<=$i;$j++){
-        if($j%3==0){
-           continue;
-        }else{
-            echo $i."*".$j."=".$i*$j." ";
-        }
-    }echo "<br>";
+    for($j=1;$j<=$i;$j++){
+        if($j%3==0)continue;
+        echo $i."*".$j."=".$i*$j." ";
+    }
+    echo "<br>";
 }
 
+for ($i=1;$i<=9;$i++){
+    for ($j=1;$j<=2;$j++){
+        if($i==1&&$j==1) continue;
+        echo $i.'*'.$j.'='.$i*$j." ";
+    }
+    echo "<br>";
+}
 
+for ($i=1;$i<=9;$i++){
+    for ($j=1;$j<=2;$j++){
+        if($i==1&&$j==1) continue;
+        if($i>=4)break;
+        echo $i.'*'.$j.'='.$i*$j." ";
+    }
+    echo "<br>";
+}
 
 ?>
